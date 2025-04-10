1590c7f9349a0a7b8420f3a46e019260d095932a
cgm <501704164@qq.com>
2025-04-10 17:42:16 +0800

    更新 box scale

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from models import YOLO
 
 if __name__ == '__main__':
     model = YOLO("yolov5s.yaml", task='detect')
-    # model = YOLO("yolov5s.yaml", task='detect')
-    # model = YOLO("./runs/detect/train/version_29/checkpoints/last.pt", task='detect')
+    # model = YOLO("./runs/detect/train/version_16/checkpoints/last.pt", task='detect')
     model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=100, batch=6, box=0.05, cls=0.5, obj=1)
 
     # model = YOLO("./runs/detect/train/version_1/checkpoints/last.pt")
