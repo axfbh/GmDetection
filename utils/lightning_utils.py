a90b81328e3677287a5021a31890ce9fdfe1eb24
cgm <501704164@qq.com>
2025-04-20 22:57:19 +0800

    更新 prograss bar

--- utils/lightning_utils.py
+++ utils/lightning_utils.py
@@ -1,5 +1,5 @@
 import sys
-from lightning.pytorch.callbacks import TQDMProgressBar
+from lightning.pytorch.callbacks import TQDMProgressBar, ProgressBar
 from lightning.pytorch.callbacks.progress.tqdm_progress import Tqdm, convert_inf
 from typing import Any, OrderedDict
 from numbers import Number
@@ -13,123 +13,119 @@ class LitTqdm(Tqdm):
     def format_meter(*args, **kwargs):
         item = tqdm.format_meter(**kwargs)
         item = item.replace(',', ' ')
-        item = item.replace('=', ': ')
         return item
 
+    def set_description(self, desc=None, refresh=True):
+        """
+        Set/modify description of the progress bar.
 
-class LitProgressBar(TQDMProgressBar):
-    BAR_FORMAT = "{desc} [{n_fmt}/{total_fmt}]{postfix}  剩余:{remaining}"
+        Parameters
+        ----------
+        desc  : str, optional
+        refresh  : bool, optional
+            Forces refresh [default: True].
+        """
+        self.desc = desc
+        if refresh:
+            self.refresh()
 
-    def init_train_tqdm(self) -> LitTqdm:
-        """Override this to customize the tqdm bar for training."""
-        return LitTqdm(
-            desc=self.train_description,
-            position=(2 * self.process_position),
-            disable=self.is_disabled,
-            leave=True,
-            dynamic_ncols=True,
-            file=sys.stdout,
-            smoothing=0,
-            bar_format=self.BAR_FORMAT,
-        )
+    def set_postfix(self, ordered_dict=None, refresh=True, **kwargs):
+        """
+        Set/modify postfix (additional stats)
+        with automatic formatting based on datatype.
 
-    def on_train_epoch_start(self, trainer, *_: Any) -> None:
-        self.train_progress_bar.reset(convert_inf(self.total_train_batches))
-        self.train_progress_bar.initial = 0
-        self.train_progress_bar.set_description(f"Epoch [{trainer.current_epoch}]")
+        Parameters
+        ----------
+        ordered_dict  : dict or OrderedDict, optional
+        refresh  : bool, optional
+            Forces refresh [default: True].
+        kwargs  : dict, optional
+        """
+        # Sort in alphabetical order to be more deterministic
+        postfix = OrderedDict([] if ordered_dict is None else ordered_dict)
+        for key in sorted(kwargs.keys()):
+            postfix[key] = kwargs[key]
+        # Preprocess stats according to datatype
+        for key in postfix.keys():
+            # Number: limit the length of the string
+            if isinstance(postfix[key], Number):
+                postfix[key] = self.format_num(postfix[key])
+            # Else for any other type, try to get the string conversion
+            elif not isinstance(postfix[key], str):
+                postfix[key] = str(postfix[key])
+            # Else if it's a string, don't need to preprocess anything
+        # Stitch together to get the final postfix
+        self.postfix = '  '.join(key + ': ' + postfix[key].strip()
+                                 for key in postfix.keys())
+        if refresh:
+            self.refresh()
+
+
+class LitProgressBar(ProgressBar):
+    def __init__(self, refresh_rate=1):
+        super().__init__()  # don't forget this :)
+        self.enable = True
+        self.refresh_rate = refresh_rate
+
+    def disable(self):
+        self.enable = False
 
     def get_metrics(self, trainer, model):
-        items = super().get_metrics(trainer, model)
-        items.pop("v_num", None)
-        return items
+        metrics_step = trainer._results.metrics(True)['pbar']
+        metrics_step = {k[:-5]: v for k, v in metrics_step.items()}
+        metrics_epoch = trainer._results.metrics(False)['pbar']
+        metrics_epoch = {k[:-6]: v for k, v in metrics_epoch.items()}
 
-# class LitProgressBar(ProgressBar):
-#
-#     def __init__(self, refresh_rate=1):
-#         super().__init__()  # don't forget this :)
-#         self.enable = True
-#         self.refresh_rate = refresh_rate
-#
-#     def disable(self):
-#         self.enable = False
-#
-#     def get_metrics(self, trainer, model):
-#         items = super().get_metrics(trainer, model)
-#         items.pop("v_num", None)
-#         return items
-#
-#     def get_meters(self, trainer, pl_module):
-#         meters = self.get_metrics(trainer, pl_module)
-#
-#         meters_mean = self.get_meters_mean(trainer)
-#
-#         meters.pop("v_num", None)
-#
-#         loss_str = []
-#
-#         for name, meter in meters.items():
-#             if name.endswith('step'):
-#                 name = name[:-5]
-#                 if name.startswith('loss') or name.endswith('loss') and len(meters_mean) > 0:
-#                     loss_str.append(
-#                         "{}: {:.4f} ({:.4f})".format(name, meter, meters_mean[name])
-#                     )
-#                 else:
-#                     loss_str.append(
-#                         "{}: {:.4f} ".format(name, meter)
-#                     )
-#
-#         return '  '.join(loss_str)
-#
-#     def get_meters_mean(self, trainer) -> dict:
-#         meters_name = {}
-#         for result in trainer._results.result_metrics:
-#             name = result.meta.name
-#             value = result.value
-#             cumulated_batch_size = result.cumulated_batch_size
-#             meters_name[name] = (value / cumulated_batch_size).item()
-#         return meters_name
-#
-#     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
-#         super().on_train_batch_end(trainer, pl_module, outputs, batch, batch_idx)  # don't forget this :)
-#
-#         if batch_idx % self.refresh_rate == 0 or (batch_idx == self.total_train_batches - 1):
-#             MB = 1024.0 * 1024.0 * 1024.0
-#
-#             delimiter = '  '
-#
-#             space_fmt = ':' + str(len(str(batch_idx))) + 'd'
-#
-#             log_msg = delimiter.join([
-#                 'Epoch: [{}]'.format(trainer.current_epoch),
-#                 '[{0' + space_fmt + '}/{1}]',
-#                 '{meters}',
-#                 'max mem: {memory:.2f} GB'
-#             ])
-#
-#             print(log_msg.format(batch_idx,
-#                                  self.total_train_batches,
-#                                  meters=self.get_meters(trainer, pl_module),
-#                                  memory=torch.cuda.max_memory_allocated() / MB))
-#
-#     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
-#         super(LitProgressBar, self).on_validation_batch_end(trainer, pl_module, outputs, batch, batch_idx,
-#                                                             dataloader_idx)
-#         if batch_idx % self.refresh_rate == 0 or (batch_idx == self.total_val_batches - 1):
-#             MB = 1024.0 * 1024.0 * 1024.0
-#
-#             delimiter = '  '
-#
-#             space_fmt = ':' + str(len(str(batch_idx))) + 'd'
-#
-#             log_msg = delimiter.join([
-#                 'Val: [{}]'.format(trainer.current_epoch),
-#                 '[{0' + space_fmt + '}/{1}]',
-#                 '{meters}',
-#                 'max mem: {memory:.2f} GB'
-#             ])
-#
-#             print(log_msg.format(batch_idx,
-#                                  self.total_val_batches,
-#                                  meters=self.get_meters(trainer, pl_module),
-#                                  memory=torch.cuda.max_memory_allocated() / MB))
+        loss_str = []
+
+        for name, meter in metrics_step.items():
+            if name.startswith('loss') or name.endswith('loss') and len(metrics_epoch) > 0:
+                loss_str.append(
+                    "{}: {:.4f} ({:.4f})".format(name, meter, metrics_epoch[name])
+                )
+            else:
+                loss_str.append(
+                    "{}: {:.4f} ".format(name, meter)
+                )
+
+        return '  '.join(loss_str)
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        if batch_idx % self.refresh_rate == 0 or (batch_idx == self.total_train_batches - 1):
+            MB = 1024.0 * 1024.0 * 1024.0
+
+            delimiter = '  '
+
+            space_fmt = ':' + str(len(str(batch_idx))) + 'd'
+
+            log_msg = delimiter.join([
+                'Epoch: [{}]'.format(trainer.current_epoch),
+                '[{0' + space_fmt + '}/{1}]',
+                '{meters}',
+                'max mem: {memory:.2f} GB'
+            ])
+
+            print(log_msg.format(batch_idx,
+                                 self.total_train_batches,
+                                 meters=self.get_metrics(trainer, pl_module),
+                                 memory=torch.cuda.max_memory_allocated() / MB))
+
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
+        if batch_idx % self.refresh_rate == 0 or (batch_idx == self.total_val_batches - 1):
+            MB = 1024.0 * 1024.0 * 1024.0
+
+            delimiter = '  '
+
+            space_fmt = ':' + str(len(str(batch_idx))) + 'd'
+
+            log_msg = delimiter.join([
+                'Val: [{}]'.format(trainer.current_epoch),
+                '[{0' + space_fmt + '}/{1}]',
+                '{meters}',
+                'max mem: {memory:.2f} GB'
+            ])
+
+            print(log_msg.format(batch_idx,
+                                 self.total_val_batches,
+                                 meters=self.get_metrics(trainer, pl_module),
+                                 memory=torch.cuda.max_memory_allocated() / MB))
