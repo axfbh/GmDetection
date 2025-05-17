94a7782532918bf3dfaee6de4f68b58ccc205c57
cgm <501704164@qq.com>
2025-05-17 13:14:54 +0800

    upload

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     # model = YOLO("yolov5s.yaml", task='detect')
     # model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=100, batch=8, box=0.05, cls=0.5, obj=1)
 
-    # model = YOLO("yolov8s.yaml", task='detect')
-    # model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=100, batch=8, box=7.5, cls=0.5, dfl=1.5)
+    model = YOLO("yolov8s.yaml", task='detect')
+    model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=100, batch=8, box=7.5, cls=0.5, dfl=1.5)
 
     # model = YOLO("./runs/detect/train/version_58/checkpoints/best.pt")
     # model.val(data="./cfg/datasets/coco.yaml", workers=3, device='0', batch=16, conf=0.001, iou=0.7, max_det=300)
 
-    model = DETR("detr.yaml", task='detect')
-    model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=400, batch=24)
+    # model = DETR("detr.yaml", task='detect')
+    # model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=400, batch=24)
