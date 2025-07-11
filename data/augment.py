9b55828846d6b257fd7963324b73489e30c04f84
cgm <501704164@qq.com>
2025-07-11 11:23:12 +0800

    update augment

--- data/augment.py
+++ data/augment.py
@@ -22,9 +22,12 @@ class LongestMaxSize:
             A.LongestMaxSize(max_size=max_size, interpolation=interpolation, always_apply=always_apply, p=p)
         ]
 
-        self.fit_transform = A.Compose(T, A.BboxParams(format=format, label_fields=['labels']))
+        self.fit_transform = A.Compose(T)
+        self.fit_transform_box = A.Compose(T, A.BboxParams(format=format, label_fields=['labels']))
 
     def __call__(self, *args, **kwargs):
+        if 'bboxes' in kwargs.keys():
+            return self.fit_transform_box(*args, **kwargs)
         return self.fit_transform(*args, **kwargs)
 
 
@@ -35,19 +38,20 @@ class Normalize:
                  max_pixel_value: Union[float, None] = 255.0,
                  normalization: Literal[
                      "standard", "image", "image_per_channel", "min_max", "min_max_per_channel"] = "standard",
-                 always_apply: bool = False,
                  p: float = 1.0):
         T = [
-            A.Normalize(mean, std, max_pixel_value, normalization, always_apply, p),
+            A.Normalize(mean, std, max_pixel_value, normalization, p),
             ToTensorV2()
         ]
         self.fit_transform = A.Compose(T)
 
     def __call__(self, *args, **kwargs):
-        image = self.fit_transform(image=kwargs['image'])['image']
-        target = {"boxes": torch.as_tensor(kwargs['bboxes']),
-                  "labels": torch.as_tensor(kwargs['labels'], dtype=torch.long)}
-        return image, target
+        if 'bboxes' in kwargs.keys():
+            image = self.fit_transform(image=kwargs['image'])['image']
+            target = {"boxes": torch.as_tensor(kwargs['bboxes']),
+                      "labels": torch.as_tensor(kwargs['labels'], dtype=torch.long)}
+            return image, target
+        return self.fit_transform(image=kwargs['image'])['image']
 
 
 class Mosaic:
