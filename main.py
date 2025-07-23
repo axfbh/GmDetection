1a057b0155cfc5e3b0b47de3f919f4a5c121df15
cgm <501704164@qq.com>
2025-07-23 17:39:59 +0800

    update file

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ if __name__ == '__main__':
     # model = YOLO("./runs/detect/train/version_58/checkpoints/best.pt")
     # model.val(data="./cfg/datasets/coco.yaml", workers=3, device='0', batch=16, conf=0.001, iou=0.7, max_det=300)
 
-    # model = DETR("detr.yaml", task='detect')
-    # model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=400, batch=8)
+    model = DETR("detr.yaml", task='detect')
+    model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=400, batch=8)
 
-    model = LITESEG('liteseg.yaml', task='segment')
-    model.train(data="./cfg/datasets/coco-seg.yaml", device='0', imgsz=640, epochs=400, batch=8)
+    # model = LITESEG('liteseg.yaml', task='segment')
+    # model.train(data="./cfg/datasets/coco-seg.yaml", device='0', imgsz=640, epochs=400, batch=8)
 
