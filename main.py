e8a9c6e5c732246bffbbc55c0834559e3187af77
cgm <501704164@qq.com>
2025-07-24 16:42:52 +0800

    update test files

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     model = DETR("detr.yaml", task='detect')
     model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=400, batch=8,
                 optimizer='AdamW',
-                lr0=0.0001,
+                lr0=0.001,
                 momentum=0.9,
                 lrf=0.01)
 
