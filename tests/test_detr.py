10fa3c7f6faf2a8be20fe29b6caa3a271ee7d0b4
cgm <501704164@qq.com>
2025-07-25 10:29:38 +0800

    update files

--- tests/test_detr.py
+++ tests/test_detr.py
@@ -4,9 +4,14 @@ if __name__ == '__main__':
     model = DETR("detr.yaml", task='detect')
     model.train(data="./cfg/datasets/coco.yaml", device='0', imgsz=640, epochs=400, batch=8,
                 optimizer='AdamW',
-                lr0=0.001,
+                lr0=0.0001,
+                lrb=0.00001,
                 momentum=0.9,
-                lrf=0.01)
+                warmup_bias_lr=0.001,
+                warmup_weight_lr=0.01,
+                warmup_weight_backbone_lr=0.001,
+                warmup_momentum=0.88,
+                lrf=1)
 
     # model = DETR("./runs/detect/train/version_58/checkpoints/best.pt")
     # model.val(data="./cfg/datasets/coco.yaml", workers=3, device='0', batch=16, conf=0.001, iou=0.7, max_det=300)
