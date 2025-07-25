ba61a1ef970849340305037e74f01ab091be9a6a
cgm <501704164@qq.com>
2025-07-25 15:00:16 +0800

    update files

--- tests/test_yolo.py
+++ tests/test_yolo.py
@@ -8,7 +8,9 @@ if __name__ == '__main__':
     # model.train(data="./tests/coco.yaml", device='0', imgsz=640, epochs=100, batch=8, box=0.05, cls=0.5, obj=1)
 
     model = YOLO("yolov8s.yaml", task='detect')
-    model.train(data="./tests/coco.yaml", device='0', imgsz=640, epochs=100, batch=8, box=7.5, cls=0.5, dfl=1.5)
+    model.train(data="./tests/coco.yaml", device='0', imgsz=640, epochs=100, batch=8, box=7.5, cls=0.5, dfl=1.5,
+                warmup_weight_lr=0,
+                warmup_weight_backbone_lr=0)
 
     # model = YOLO("./runs/detect/train/version_0/checkpoints/last.pt", task='detect')
     # model.train(data="./cfg/datasets/coco.yaml", device='0', resume=True, imgsz=640, epochs=100, batch=8, box=7.5,
